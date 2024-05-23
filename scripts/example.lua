525e8456e1c1606c2b18c076ae8a644880f3ff92
Matthew Ibbetson <mmibbetson@gmail.com>
2024-05-23 13:53:05 +0200

    add example deno script

--- scripts/example.lua
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/luajit
-
--- This is an example for the scripts directory
-print("Scripts are visible in path!")
---
+++ scripts/hellodeno.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S deno run --ext=ts
+
+/**
+ * You can remove the .ts extension from the file it you want to call a
+ * particular script without the file extension, but this will ruin syntax
+ * highlighting when editing it. Best for very stable scripts.
+ */
+
+console.log("Hello from Deno!");
